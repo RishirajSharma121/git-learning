ba4a618dfa0f9dc4d217807c509a4c76eac95733
Rishiraj Sharma <rishiraj.sharma@novelveritas.com>
2023-05-24 12:09:55 +0530

    add github

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const express=require("express");//creat express
 const index = express();// create express object
 index.listen(3000,()=>console.log ('server is listening'));
+index.listen(3000,()=>console.log ('server is listening'));index.listen(3000,()=>console.log ('server is listening'));
